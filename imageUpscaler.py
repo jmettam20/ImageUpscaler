f8a6f11a58357fe93dd7d844feb58f2781e7e459
Josh Mettam <78258956+jmettam20@users.noreply.github.com>
2023-04-19 20:01:57 +0100

    Add files via upload

--- imageUpscaler.py
+++ imageUpscaler.py
@@ -1,21 +1,34 @@
+from flask import Flask, request, jsonify
 from PIL import Image, ImageEnhance
 
-# Open the image file
-img = Image.open('/Users/joshmettam/Documents/Programming/test.tga')
+# new flask instance
+app = Flask(__name__)
 
-# Calculate the new size of the image
-new_width = img.width * 12
-new_height = img.height * 12
-new_size = (new_width, new_height)
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-# Resize the image
-resized_img = img.resize(new_size, Image.BILINEAR)
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the uploaded file from the request object
+    file = request.files['image']
+    # Open the image file
+    img = Image.open(file)
+    # Calculate the new size of the image
+    new_width = img.width * 12
+    new_height = img.height * 12
+    new_size = (new_width, new_height)
+    # Resize the image
+    resized_img = img.resize(new_size, Image.BILINEAR)
+    # Create an object to enhance the image
+    enhancer = ImageEnhance.Sharpness(resized_img)
+    # Enhance the image
+    enhanced_img = enhancer.enhance(3.0)
+    # Save the resized image
+    output_path = 'output.tga'
+    enhanced_img.save(output_path)
+    # Return the path of the output image as a JSON response
+    return jsonify({'output_path': output_path})
 
-# Create an object to enhance the image
-enhancer = ImageEnhance.Sharpness(img)
-
-# Enhance the image
-enhanced_img = enhancer.enhance(3.0)
-
-# Save the resized image
-enhanced_img.save('/Users/joshmettam/Documents/Programming/output.tga')
+if __name__ == '__main__':
+    app.run(debug=True)
